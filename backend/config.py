0cd250e7ada8913a75e43cbde895a2b5584bbce6
sarthakgupta04 <gsarthak283@gmail.com>
2024-04-22 21:17:19 +0530

    fourth

--- backend/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.urandom(24)
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://sarthak:sg123@localhost/investedge'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
